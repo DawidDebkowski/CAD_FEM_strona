cb1a0e9bffafc1f40886ec8877c997c15a4cd0db
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-03-29 13:56:48 +0100

    czyszczenie pod build

--- cad-fem-strona/src/main.jsx
+++ cad-fem-strona/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import People from './People.jsx'
 import Header from './Header.jsx'
