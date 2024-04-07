e76ac6db1d8e4d7de429eb5dccc8ec01fccf5ec1
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-04-07 11:41:34 +0200

    restrukturyzacja dla sciezek
    i React Router

---
+++ cad-fem-strona/src/App.jsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import './Pages/HomePage.jsx'
+import './Pages/AboutUs.jsx'
+import AboutUs from "./Pages/AboutUs.jsx";
+import HomePage from "./Pages/HomePage.jsx";
+
+export default function App()
+{
+  return(
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<HomePage/>}/>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/Home" element={<HomePage/>}/>
+          <Route path="/AboutUs" element={<AboutUs/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
