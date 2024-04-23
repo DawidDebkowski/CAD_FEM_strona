a37ab90b80fb7a84f6b40b20c1b0448347810800
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-04-23 16:09:13 +0200

    sideways projectBox

--- cad-fem-strona/src/Components/ProjectBox.jsx
+++ cad-fem-strona/src/Components/ProjectBox.jsx
@@ -4,11 +4,13 @@ export default function ProjectBox({title, image, desc})
 {
     return (
         <div className="projectDiv">
-
-                <img src={image} />
+            <img src={image} />
 
             <div className="projectTexts">
                 <h1>{title}</h1>
+            </div>
+            <hr/>
+            <div className="description">
                 <p>{desc}</p>
             </div>
         </div>
