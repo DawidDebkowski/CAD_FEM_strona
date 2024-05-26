dee86501af40fd3dfb4dadc82933f4387bf7503d
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-05-26 20:29:51 +0200

    singlePost input

--- src/Components/SinglePostPage/SinglePost.jsx
+++ src/Components/SinglePostPage/SinglePost.jsx
@@ -1,38 +1,34 @@
+import PropTypes from "prop-types"
 import "./singlePost.css"
 
-export default function SinglePost() {
+export default function SinglePost({project}) {
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
                 <img src="/mechanicy.png" alt="głowne zdjęcie postu" className="singlePostImg"/>
-                <h1 className="singlePostTitle">Tytuł Projektu, który będzie o taki</h1>
+                <h1 className="singlePostTitle">{project.title}</h1>
                 <div className="singlePostInfo">
                     <span className="singlePostAuthor">
-                        Autor: <b>Imie</b>
+                        Autor: <b>{project.author}</b>
                     </span>
                     <span className="singlePostDate">
-                        26.05.2024
+                        {project.date}
                     </span>
                 </div>
                 <p className="singlePostDesc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Numquam sequi corporis quae nam, id tenetur, accusantium assumenda 
-                    aliquid mollitia temporibus obcaecati sapiente quas quasi? 
-                    Delectus ducimus earum aperiam dolor provident!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Numquam sequi corporis quae nam, id tenetur, accusantium assumenda 
-                    aliquid mollitia temporibus obcaecati sapiente quas quasi? 
-                    Delectus ducimus earum aperiam dolor provident!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Numquam sequi corporis quae nam, id tenetur, accusantium assumenda 
-                    aliquid mollitia temporibus obcaecati sapiente quas quasi? 
-                    Delectus ducimus earum aperiam dolor provident!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Numquam sequi corporis quae nam, id tenetur, accusantium assumenda 
-                    aliquid mollitia temporibus obcaecati sapiente quas quasi? 
-                    Delectus ducimus earum aperiam dolor provident!
+                    {project.text}
                 </p>
             </div>
         </div>
     )
 }
+
+SinglePost.propTypes = {
+  project: PropTypes.shape({
+    author: PropTypes.string,
+    date: PropTypes.string,
+    text: PropTypes.string,
+    title: PropTypes.string
+  })
+}
+
--- src/Pages/SinglePostPage.jsx
+++ src/Pages/SinglePostPage.jsx
@@ -1,7 +1,20 @@
 import SinglePost from "../Components/SinglePostPage/SinglePost";
+export class SingleProjectInfo {
+    constructor(title, author, date, text) {
+        this.title = title;
+        this.author = author;
+        this.date = date;
+        this.text = text;
+    }
+}
 
 export default function SinglePostPage() {
+    var project = new SingleProjectInfo("Tytuł Projektu, który będzie taki",
+        "Imie",
+        "26.05.2024", 
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium! \n Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, cupiditate ratione ad rem repellat nobis magni dignissimos inventore repudiandae atque voluptatem iure eius odio quo aspernatur magnam in non laudantium!"
+    )   
     return (
-        <div><SinglePost/></div>
+        <div><SinglePost project={project}/></div>
     )
 }
