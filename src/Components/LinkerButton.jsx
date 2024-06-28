716af797c87c6f8b5ce8a321cef42c535fced74a
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-06-28 15:13:18 +0200

    fiex footer position (finally)

--- src/Components/LinkerButton.jsx
+++ src/Components/LinkerButton.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import "./linkerButton.css"
 
 export default function LinkerButton({ text, linkTo, moreClasses }) {
     return (
         <>
-            <Link to={linkTo} className={"btn " + moreClasses}>{text}</Link>
+            <Link to={linkTo} className={"linkerButton btn " + moreClasses}>{text}</Link>
         </>
     )
 }
