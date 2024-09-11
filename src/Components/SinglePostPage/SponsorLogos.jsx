97bf37d576f033b5035b3217f2aa003f348730b0
Dawid Dębkowski <45078202+DawidDebkowski@users.noreply.github.com>
2024-09-11 14:28:42 +0200

    sponsor wheel

--- src/Components/SinglePostPage/SponsorLogos.jsx
+++ src/Components/SinglePostPage/SponsorLogos.jsx
@@ -1,7 +1,24 @@
+import sponsorMock from "./../../assets/sponsor_mock.json"
+import "./sponsorLogos.css"
+
+export class Sponsor {
+    constructor(name, logoPath) {
+        this.name = name;
+        this.logoPath = logoPath;
+    }
+}
+
 function SponsorLogos() {
+    let logoWheel = [];
+    sponsorMock.forEach(sponsor => {
+        logoWheel.push((
+            <img src={sponsor.imagePath} className="sponsorLogo"/>
+        ))
+    });
     return ( 
         <div className="sponsorLogosBox">
             <h2>Sponsorzy:</h2>
+            {logoWheel}
         </div>
      );
 }
