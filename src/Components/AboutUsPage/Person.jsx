659386b6431bbdef66531e1477eebefeb5bc46fe
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-02-26 14:25:00 +0100

    better alt in AboutUs

--- src/Components/AboutUsPage/Person.jsx
+++ src/Components/AboutUsPage/Person.jsx
@@ -4,7 +4,7 @@ function Person({ name, lastName, description, imageSource }) {
     return (
         <>
             <div className="person">
-                <img src={imageSource} alt={name} />
+                <img src={imageSource} alt={"Zdjecie " + name} />
                 <h2>{name} {lastName}</h2>
                 <p>{description}</p>
             </div>
